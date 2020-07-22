e76d1726b4c3e4ed84b4d12e78735f973ce71456
Marco Pereira <marcocsp@hotmail.com>
2020-07-22 04:59:11 +0100

    Refactore app.component in order to receive the new components

--- frontend/video-player-app/src/app/app.module.ts
+++ frontend/video-player-app/src/app/app.module.ts
@@ -1,18 +1,34 @@
+import { BookmarksModule } from './features/bookmarks/bookmarks.module';
+import { HistoryModule } from './features/history/history.module';
+import { VideoViewModule } from './features/video-view/video-view.module';
+import { SearchBarModule } from './features/search-bar/search-bar.module';
+import { MaterialModule } from './core/material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HistoryComponent } from './features/history/history.component';
+import { BookmarksComponent } from './features/bookmarks/bookmarks.component';
+import { SidebarComponent } from './core/layout/sidebar/sidebar.component';
+import { MainContentComponent } from './core/layout/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainContentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    SearchBarModule,
+    VideoViewModule,
+    HistoryModule,
+    BookmarksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
